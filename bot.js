185ad2750f0b696d8df9db93343c2b6c391cff11
Rainbow-Turtle1 <arran.j.mcp@gmail.com>
2026-07-05 02:31:05 +0100

    test 6

--- bot.js
+++ bot.js
@@ -348,7 +348,7 @@ async function handleInvincileInitiative(message) {
 		const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 		for (let i = 0; i < playersInVc.length; i++) {
 			//select number card for each player then reduce that by 1
-			await message.reply(cards);
+			await message.reply("cards", cards);
 
 			const randomNumber = Math.floor(Math.random() * messages.length);
 			// use the random number to select and delete a card
